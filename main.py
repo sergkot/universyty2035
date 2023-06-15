3a3c5b5dc86c9eff8b24c8e2b2444268dd9164c2
sergkot <sergkot@bk.ru>
2023-06-15 07:09:53 +0300

    два варианта палиндрома

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ s = 'aabcca'
 
 
 def strcount1(s):
-    print('Hello)
+    print('Hello')
     for sym in set(s):
         # print(sym)
         counter = 0
@@ -24,3 +24,37 @@ def strcounter(s):
 
 
 strcounter(s)
+
+def palindrom(s):
+    # first_pos = 0
+    end_pos = len(s)
+    ItIsPalindrom = True
+    for first_pos in range(0, int(len(s)/2)):
+        end_pos -= 1
+        if s[first_pos] != s[end_pos]:
+            ItIsPalindrom = False
+            break
+    return ItIsPalindrom
+
+def palindrom_1(s):
+    first_pos = 0
+    end_pos = len(s) - 1
+    ItIsPalindrom = True
+    while first_pos < end_pos:
+        if s[first_pos] != s[end_pos]:
+            ItIsPalindrom = False
+            break
+        first_pos += 1
+        end_pos -= 1
+    return ItIsPalindrom
+
+
+
+strcount1(s)
+print(set(s))
+print(palindrom('лепсспел'))
+print(palindrom('helloworld'))
+
+print('---------------')
+print(palindrom_1('лепсспел'))
+print(palindrom_1('helloworld'))
